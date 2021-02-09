26e7e815bb56862ee5778fb28cb857c0b42155f6
ohlayman <raohao0323@sina.com>
2021-02-09 23:07:41 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+  components: {
+
+  },
 };
 </script>
 
 <style>
+
 </style>
